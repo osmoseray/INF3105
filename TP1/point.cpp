a8b68f38c434179e81ae67aca245d6e4b08fcf80
Laurent Morissette <Laurent.Morissette@gmail.com>
2013-10-14 11:03:11 -0400

    Update point.cpp

--- TP1/point.cpp
+++ TP1/point.cpp
@@ -1,47 +1,34 @@
-/*
- *Date: 13-09-10
-
-*/
-
-
 #include <assert.h>
 #include <math.h>
 #include "point.h"
 
-Point::Point(const Point& point)
-  : x(point.x), y(point.y)
-{
-}
-
-Point::Point(double _x, double _y)
+Point::Point(double _x, double _y) 
   : x(_x), y(_y)
 {
 }
 
+Point::Point(const Point& point)
+  : x(point.x), y(point.y) 
+{
+}
 
 double Point::distance(const Point& point) const {
-   double carreX=pow((point.x-x),2);
-   double carreY=pow((point.y-y),2);
-   double dist=sqrt(carreX+carreY);
-   return dist;
+    return sqrt(pow((x-point.x), 2) + pow((y-point.y),2));
 }
 
-
-std::ostream& operator << (std::ostream& os, const Point& point) {
-  os << "(" << point.x << "," << point.y << ")";
-  return os;
+ostream& operator << (ostream& os, const Point& point) {
+    os << "(" << point.x << "," << point.y << ")";
+    return os;
 }
 
-std::istream& operator >> (std::istream& is, Point& point) {
-  char po, vir, pf;
-  is >> po;
-  if(is){
-    is >> point.x >> vir >> point.y >> pf;
-    assert(po=='(');
-    assert(vir==',');
-    assert(pf==')');
-  }
-  return is;
+istream& operator >> (istream& is, Point& point) {
+    char po, vir, pf;
+    is >> po;
+    if(is){
+        is >> point.x >> vir >> point.y >> pf;
+        assert(po=='(');
+        assert(vir==',');
+        assert(pf==')');
+    }
+    return is;
 }
-
-
