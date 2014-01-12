88c8820cf60a8bfd6fe3733a773e22796fdca98b
laurentmor <laurent.morissette@gmail.com>
2014-01-11 21:55:40 -0500

    ajout de Heure 2

---
+++ exercises_livre_cpp/Heure_2_A_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Heure_2_A_2.h"
+
+using namespace std;
+int add(int x,int y,int z){
+
+	return (x+y+z);
+}
+int main()
+{
+	cout<<"Somme: "<<add(1,2,3)==6;
+	return 0;
+}
+
+	
---
+++ exercises_livre_cpp/Heure_2_A_2.h
@@ -0,0 +1,4 @@
+#ifndef HEURE_2_A_2_H
+#define HEURE_2_A_2_H
+int add(int x,int y,int z);
+#endif
