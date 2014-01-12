475d69fc476637b94776d519a54e59a6074e9179
laurentmor <laurent.morissette@gmail.com>
2014-01-11 20:42:24 -0500

    debut des ex

---
+++ exercises_livre_cpp/Heure_1_A_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Heure_1_A_1.h"
+using namespace  std
+void  sayHelloEsperanto(){
+	cout<<"saluton mondo!";
+}
+int main(int argc, char const *argv[])
+{
+  sayHelloEsperanto();	
+	return 0;
+}
