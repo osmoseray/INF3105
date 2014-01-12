8810f092643a6ede44fbef497391bac187f914df
laurentmor <laurent.morissette@gmail.com>
2014-01-12 16:45:21 -0500

    Heure 3

---
+++ exercises_livre_cpp/Heure_3_A_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+// set up width and length
+ unsigned short width = 5, length,height;
+ length = 10;
+ height = 15;
+ // create an unsigned short initialized with the
+ // result of multiplying width by length
+ unsigned short area = width * length * height;
+
+ std::cout << “Width: “ << width << “\n”;
+ std::cout << “Length: “ << length << “\n”;
+ std::cout << “height: “ << height << “\n”;
+ std::cout << “Area: “ << area << “\n”;
+ return 0;
+}
