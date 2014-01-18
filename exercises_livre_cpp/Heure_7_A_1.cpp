a57baa64976800036f2b1f70272f80891364f003
laurentmor <laurent.morissette@gmail.com>
2014-01-18 17:19:53 -0500

    Heure 7

---
+++ exercises_livre_cpp/Heure_7_A_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	char name[35];
+	cout<<"Enter name: ";
+	cin>>name;
+	cout<<endl<<"hello"<<name;
+	return 0;
+}
